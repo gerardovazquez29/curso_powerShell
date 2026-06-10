022e0d9d94799af553aba95281e94dec538ff267
gerardovazquez29 <gerardovazquez2903@hotmail.com>
2026-06-10 17:51:41 -0600

    nuevo

--- ejercicios/ejercicio3.ps1
+++ ejercicios/ejercicio3.ps1
@@ -36,16 +36,24 @@ Debe guardar en un archivo auditoria.log algo como: [08/06/2026] [ERROR] El serv
 function RegistrarActividad {
     param (
         [string]$mensaje,
-        [validaset("info","Warning","Error")]
-        [string]$tipo = "info"
+        [string]$tipo    # "info","Warning","Error" 
     )
+    $fecha = Get-Date -Format "dd/MM/yyyy HH:mm:ss"
+    $tipo = $tipo.ToUpper()
+    $linea = "[$fecha] [$tipo] $mensaje"
     $ruta = "C:\Users\T-City\OneDrive\Documentos\PowerShellScripts\Auditoria.log"
-    $fecha = Get-Date -Format "dd/mm/yyyy HH:mm:ss"
-    Add-Content -Path $ruta -Value "[$fecha] [$tipo] $mensaje"
-    Write-Host "[$tipo] $mensaje" -ForegroundColor 
+    Add-Content -Path $ruta -Value $linea
+    Write-Host $linea 
 }
+RegistrarActividad -mensaje "El Servicio DNS inicio correctamente" -tipo "info"
+RegistrarActividad -mensaje "El disco C: esta al 90% de uso"       -tipo "Warning"
+RegistrarActividad -mensaje "El servicio DHCP fallo"               -tipo "Error"
 
-
+<#
+[10/06/2026 16:45:01] [INFO] El Servicio DNS inicio correctamente
+[10/06/2026 16:45:01] [WARNING] El disco C: esta al 90% de uso
+[10/06/2026 16:45:01] [ERROR] El servicio DHCP fallo
+#>
 
 Write-Host "================================================================"
 
@@ -57,3 +65,33 @@ Intente obtener el servicio con Try.
 Si el servicio no existe (Catch), use su función Registrar-Actividad para guardar el error en el log.
 Si el servicio existe, guarde un log de tipo Info.
 #>
+
+function VerificadorServicios {
+    param (
+        [Parameter(Mandatory, HelpMessage = "Escribe los Servicios a probar")]  
+        [string[]]$servicios    
+        )
+
+        foreach ($servicio in $servicios) {
+            try {
+                $estado = (Get-Service -Name $servicio -ErrorAction Stop).Status
+                RegistrarActividad -mensaje "El servicio $servicio fue consultado correctamente. Estado: $estado" -tipo "info"
+                $respuesta = if ($estado) {$estado} else {"No encontrado"} 
+                [PSCustomObject]@{
+                    servicio = $servicio
+                    responde = $respuesta  
+                }    
+            }
+            catch {
+                RegistrarActividad -mensaje "Error al consultar el servicio $servicio.No fue encontrado o no pudo obtenerse. Detalle: $_" -tipo "Error"
+
+                [PSCustomObject]@{
+                    servicio = $servicio
+                    responde = "No encontrado"  #if ($estado) {$estado} else {"No encontrado"}   
+                }
+            }
+            
+        }
+}
+VerificadorServicios -servicios @("Apache2.4", "Appinfo", "AppXSvc", "AudioEndpointBuilder", "servicioFallo") | Format-Table -AutoSize
+
